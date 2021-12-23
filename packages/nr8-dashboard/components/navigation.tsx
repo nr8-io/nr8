4b71423a9f3984d31711040217a0532a7bf56a5b
Michael Mitchell <mike@topvine.co>
2021-12-23 12:51:13 +0200

    work on navigation

--- packages/nr8-dashboard/components/navigation.tsx
+++ packages/nr8-dashboard/components/navigation.tsx
@@ -1,14 +1,161 @@
-import { Box } from '@chakra-ui/react'
+import { FunctionComponent } from 'react'
+
+import {
+  Box,
+  Flex,
+  IconButton,
+  Button,
+  Image,
+  HStack,
+  Collapse,
+  useColorMode,
+  useColorModeValue,
+  useBreakpointValue,
+  useDisclosure
+} from '@chakra-ui/react'
+
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from '@chakra-ui/icons'
 
 //
-import ColorToggle from './buttons/color-toggle'
+import DesktopNav from './navigation/desktop'
+import MobileNav from './navigation/mobile'
 
 //
-export default function () {
+export interface NavItem {
+  label: string
+  subLabel?: string
+  children?: Array<NavItem>
+  href?: string
+}
+
+const navItems: Array<NavItem> = [
+  {
+    label: 'Inspiration',
+    children: [
+      {
+        label: 'Explore Design Work',
+        subLabel: 'Trending Design to inspire you',
+        href: '#'
+      },
+      {
+        label: 'New & Noteworthy',
+        subLabel: 'Up-and-coming Designers',
+        href: '#'
+      }
+    ]
+  },
+  {
+    label: 'Find Work',
+    children: [
+      {
+        label: 'Job Board',
+        subLabel: 'Find your dream design job',
+        href: '#'
+      },
+      {
+        label: 'Freelance Projects',
+        subLabel: 'An exclusive list for contract work',
+        href: '#'
+      }
+    ]
+  },
+  {
+    label: 'Learn Design',
+    href: '#'
+  },
+  {
+    label: 'Hire Designers',
+    href: '#'
+  }
+]
+
+/**
+ *
+ */
+const Navigation: FunctionComponent = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+  const { isOpen, onToggle } = useDisclosure()
+
   return (
-    <>
-      <Box>I am nav</Box>
-      <ColorToggle />
-    </>
+    <Box>
+      <Flex
+        align={'center'}
+        bg={useColorModeValue('white', 'gray.800')}
+        borderBottom={1}
+        borderColor={useColorModeValue('gray.200', 'gray.900')}
+        borderStyle={'solid'}
+        color={useColorModeValue('gray.600', 'white')}
+        minH={'60px'}
+        px={{ base: 4 }}
+        py={{ base: 2 }}
+      >
+        <Flex
+          display={{ base: 'flex', md: 'none' }}
+          flex={{ base: 1, md: 'auto' }}
+          ml={{ base: -2 }}
+        >
+          <IconButton
+            aria-label={'Toggle Navigation'}
+            icon={
+              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+            }
+            onClick={onToggle}
+            variant={'ghost'}
+          />
+        </Flex>
+
+        <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+          <Box
+            textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+            fontFamily={'heading'}
+            color={useColorModeValue('gray.800', 'white')}
+          >
+            <Image
+              src={useColorModeValue('/logo-purple.png', '/logo-white.png')}
+              alt={'Topvine Consulting EOOD'}
+              height={'32px'}
+            />
+          </Box>
+
+          <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+            <DesktopNav navItems={navItems} />
+          </Flex>
+        </Flex>
+
+        <HStack flex={{ base: 1, md: 0 }} justify={'flex-end'} spacing={6}>
+          <Button onClick={toggleColorMode}>
+            {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+          </Button>
+          <Button
+            as={'a'}
+            fontSize={'sm'}
+            fontWeight={400}
+            variant={'link'}
+            href={'#'}
+          >
+            Sign In
+          </Button>
+          <Button
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize={'sm'}
+            fontWeight={600}
+            color={'white'}
+            bg={'pink.400'}
+            href={'#'}
+            _hover={{
+              bg: 'pink.300'
+            }}
+          >
+            Sign Up
+          </Button>
+        </HStack>
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity>
+        <MobileNav navItems={navItems} />
+      </Collapse>
+    </Box>
   )
 }
+
+export default Navigation
