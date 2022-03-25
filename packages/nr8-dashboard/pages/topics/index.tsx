04444f76d2d9886a89cc5be4d220bb50cd7bce21
Michael Mitchell <mike@topvine.co>
2022-03-25 16:05:48 +0200

    experiements with slate for editable fields

--- packages/nr8-dashboard/pages/topics/index.tsx
+++ packages/nr8-dashboard/pages/topics/index.tsx
@@ -1,4 +1,14 @@
+import { useCallback, useState } from 'react'
 import type { NextPage } from 'next'
+import {
+  createEditor,
+  Editor,
+  BaseEditor,
+  Descendant,
+  Text as SlateText,
+  Transforms
+} from 'slate'
+import { Slate, Editable, ReactEditor, withReact } from 'slate-react'
 import {
   Alert,
   AlertTitle,
@@ -27,6 +37,18 @@ import {
 //
 import DocumentLayout from '../../components/layouts/document'
 import Link from '../../components/link'
+
+//
+type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomText = { text: string }
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
 
 //
 export { getServerSideProps } from '../../providers/chakra'
@@ -101,6 +123,96 @@ export const PreviousButton: React.FC<ButtonProps> = (props) => {
   )
 }
 
+const DefaultElement = (props) => {
+  return <p {...props.attributes}>{props.children}</p>
+}
+
+// Define a React component renderer for our code blocks.
+const CodeElement = (props) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
+}
+
+const Leaf = (props) => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
+    >
+      {props.children}
+    </span>
+  )
+}
+
+const EditText = () => {
+  const [editor] = useState(() => withReact(createEditor()))
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }]
+    }
+  ])
+
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
+
+  return (
+    <Slate
+      editor={editor}
+      value={value}
+      onChange={(newValue) => setValue(newValue)}
+    >
+      <Editable
+        renderElement={renderElement}
+        onKeyDown={(event) => {
+          if (!event.ctrlKey) {
+            return
+          }
+
+          switch (event.key) {
+            // When "`" is pressed, keep our existing code block logic.
+            case '`': {
+              event.preventDefault()
+              const [match] = Editor.nodes(editor, {
+                match: (n) => n.type === 'code'
+              })
+              Transforms.setNodes(
+                editor,
+                { type: match ? 'paragraph' : 'code' },
+                { match: (n) => Editor.isBlock(editor, n) }
+              )
+              break
+            }
+
+            // When "B" is pressed, bold the text in the selection.
+            case 'b': {
+              event.preventDefault()
+              console.log(event)
+              Transforms.setNodes(
+                editor,
+                { bold: true },
+                // Apply it to text nodes, and split the text node up if the
+                // selection is overlapping only part of it.
+                { match: (n) => SlateText.isText(n), split: true }
+              )
+              break
+            }
+          }
+        }}
+      />
+    </Slate>
+  )
+}
+
 const Page: NextPage = () => {
   return (
     <DocumentLayout>
@@ -155,6 +267,7 @@ const Page: NextPage = () => {
           laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor
           eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.
         </Text>
+        <EditText />
       </Box>
 
       <Flex as="footer" mt={16}>
